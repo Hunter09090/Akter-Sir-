db6c0539283d36fecab31415d0d23b990037b7b9
Hunter09090 <aakterhossen80@gmail.com>
2026-07-20 17:46:23 +0600

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,1 +1,19 @@
 
+/* =====================================
+   File : firebase-config.js
+===================================== */
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDZox_EV7PlLBUq6KbPgyckLsfThEu9PfA",
+    authDomain: "akter-sir.firebaseapp.com",
+    projectId: "akter-sir",
+    storageBucket: "akter-sir.firebasestorage.app",
+    messagingSenderId: "1078263976979",
+    appId: "1:1078263976979:web:7866159f20e6299a2d5be6",
+    measurementId: "G-HB8PMMFJMJ"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const auth = firebase.auth();
+const db = firebase.firestore();
